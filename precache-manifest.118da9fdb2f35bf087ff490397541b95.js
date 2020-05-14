32cb136b9ea481cdb83bf414ac905e41663da454
Sintean Andreea <sintean_andreea@yahoo.com>
2020-05-14 14:34:27 +0300

    Updates

---
+++ precache-manifest.118da9fdb2f35bf087ff490397541b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5cd7f9d11133a69724b7db89c4b7a8e",
+    "url": "/licence/index.html"
+  },
+  {
+    "revision": "97673bffd1cfb8a20cbb",
+    "url": "/licence/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "33808d6cc8db7da3f39d",
+    "url": "/licence/static/css/main.e56a1db3.chunk.css"
+  },
+  {
+    "revision": "97673bffd1cfb8a20cbb",
+    "url": "/licence/static/js/2.eecc4295.chunk.js"
+  },
+  {
+    "revision": "33808d6cc8db7da3f39d",
+    "url": "/licence/static/js/main.95db9d24.chunk.js"
+  },
+  {
+    "revision": "03c6395553fa36d02b5c",
+    "url": "/licence/static/js/runtime~main.4fe31732.js"
+  }
+]);
